303c7c457547d14ccafddd10832081101c994e32
pknitrr <98548863+pknitrr@users.noreply.github.com>
2025-02-05 18:08:53 +0530

    Create casting.py

---
+++ casting.py
@@ -0,0 +1,21 @@
+x = int(1)   # x will be 1
+print(x)
+
+y = int(2.8) # y will be 2
+print(y)
+
+z = int("3") # z will be 3
+print(z)
+
+x = float(1) ,   print(x)   # x will be 1.0
+y = float(2.8),  print(y)   # y will be 2.8
+z = float("3"),  print(z)   # z will be 3.0
+w1 = float("4.2") 
+print(w1)   # w will be 4.2
+
+p = str("s1") 
+print(p)      # x will be 's1'
+q = str(2)   
+print(q)      # y will be '2'
+r = str(3.0)  
+print(r)     # z will be '3.0'
