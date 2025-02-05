e9349bbcd1a9a986c154197df4eeab1cd29c4a58
pknitrr <98548863+pknitrr@users.noreply.github.com>
2025-02-05 15:45:47 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,68 @@
+x = 5
+x= 'stri'
+y="stree"
+print(x)
+print(y)
+
+#Casting variables
+
+a =5
+b=7.3
+c = 7
+i = int(b)
+f = float(a)
+s = str(c)
+print("a=",a)
+print("b=",b)
+print("i=",i)
+print("f=",f)
+print("a=",c)
+print("a=",s)
+
+
+#2myvar = "John"
+#my-var = "John"
+my_var = "John"
+print(my_var)
+
+#One Value to Multiple Variables
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+#Many Values to Multiple Variables
+x, y, z = "Orange", "Banana", "Cherry"
+print(x,y,z)
+
+#Unpack a Collection
+fruits = ["apple", "banana", "cherry"]
+print(fruits)
+p, q, r = fruits
+print(p)
+print(q)
+print(r)
+
+x = "Python"
+y = "is"
+z = "awesome"
+print(x,y,z)
+
+x = "Python"
+y = "is"
+z = "awesome"
+print(x+y+z)
+
+x = 5
+y = 10
+print(x + y)
+
+#this will throw an error
+x = 5
+y = "John"
+print(x + y)
+
+#this will not throw an error
+x = 5
+y = "John"
+print(x, y)
